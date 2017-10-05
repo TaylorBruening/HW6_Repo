65c432dffb3808d15f11264c8eae7e1729d5590d
Austin-Crabtree <ac732213@ohio.edu>
2017-10-05 11:20:27 -0400

    Updated main.cc

--- main.cc
+++ main.cc
@@ -3,11 +3,19 @@
 // November 23, 2015
 // Project7
 
+/*! 
+	@file main.cc
+	@brief Main function to run othello
+	@author 
+*/
+
 #include "game.h"
 #include "othello.h"
 using namespace main_savitch_14;
 
-
+/*!
+	@brief 
+*/
 int main()
 {
 	Othello theGame;
